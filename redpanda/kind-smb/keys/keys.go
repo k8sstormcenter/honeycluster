4a43af0a21ada70f14bfdbb79c0137e889d522bc
entlein <einentlein@gmail.com>
2024-03-10 15:52:14 +0100

    fix: adding proper hashes for kind-demo but keeping it human readable

--- redpanda/kind-smb/keys/keys.go
+++ redpanda/kind-smb/keys/keys.go
@@ -1,35 +1,45 @@
 package keys
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func hashString(s string) string {
+	hash := sha256.Sum256([]byte(s))
+	return hex.EncodeToString(hash[:])
+}
+
 var redpandaContainerId = "REDPANDA_CONTAINER_ID"
 
 var Baselinekeys = []string{
-	redpandaContainerId + "usrbinbashusrbinrpkclusterhealth",
-	redpandaContainerId + "usrbincurlsilentfailkhttpredpandasrc0redpandasrcredpandasvcclusterlocal9644v1statusready",
-	redpandaContainerId + "binshccurlsilentfailkhttp{SERVICE_NAME}redpandasrcredpandasvcclusterlocal9644v1statusready",
-	redpandaContainerId + "binshccurlsilentfailkhttp{SERVICENAME}redpandasrcredpandasvcclusterlocal9644v1statusready",
-	redpandaContainerId + "optredpandalibexecrpkclusterhealth",
-	redpandaContainerId + "binbashcrpktopiccreateextractcsv",
-	redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreateextractcsv",
-	redpandaContainerId + "usrbinbashusrbinrpktopiccreateextractcsv",
-	redpandaContainerId + "optredpandalibexecrpktopiccreateextractcsv",
-	redpandaContainerId + "binbashcrpktopiccreatebaseline",
-	redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatebaseline",
-	redpandaContainerId + "usrbinbashusrbinrpktopiccreatebaseline",
-	redpandaContainerId + "optredpandalibexecrpktopiccreatebaseline",
-	redpandaContainerId + "binbashcrpktopiccreatesignalminusbaseline",
-	redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatesignalminusbaseline",
-	redpandaContainerId + "usrbinbashusrbinrpktopiccreatesignalminusbaseline",
-	redpandaContainerId + "optredpandalibexecrpktopiccreatesignalminusbaseline",
-	redpandaContainerId + "binbashcrpktopiccreatetetragon",
-	redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatetetragon",
-	redpandaContainerId + "usrbinbashusrbinrpktopiccreatetetragon",
-	redpandaContainerId + "optredpandalibexecrpktopiccreatetetragon",
-	redpandaContainerId + "binbashcrpktopiccreatekind-smb",
-	redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatekind-smb",
-	redpandaContainerId + "usrbinbashusrbinrpktopiccreatekind-smb",
-	redpandaContainerId + "optredpandalibexecrpktopiccreatekind-smb",
-	redpandaContainerId + "binbashcmkdirptmpbaseline",
-	redpandaContainerId + "usrbinmkdirptmpbaseline",
-	redpandaContainerId + "usrbintestdtmpbaseline",
-	redpandaContainerId + "usrbintarxmfCtmpbaseline",
+	hashString(redpandaContainerId + "usrbinbashusrbinrpkclusterhealth"),
+	hashString(redpandaContainerId + "usrbincurlsilentfailkhttpredpandasrc0redpandasrcredpandasvcclusterlocal9644v1statusready"),
+	hashString(redpandaContainerId + "binshccurlsilentfailkhttp{SERVICE_NAME}redpandasrcredpandasvcclusterlocal9644v1statusready"),
+	hashString(redpandaContainerId + "binshccurlsilentfailkhttp{SERVICENAME}redpandasrcredpandasvcclusterlocal9644v1statusready"),
+	hashString(redpandaContainerId + "optredpandalibexecrpkclusterhealth"),
+	hashString(redpandaContainerId + "binbashcrpktopiccreateextractcsv"),
+	hashString(redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreateextractcsv"),
+	hashString(redpandaContainerId + "usrbinbashusrbinrpktopiccreateextractcsv"),
+	hashString(redpandaContainerId + "optredpandalibexecrpktopiccreateextractcsv"),
+	hashString(redpandaContainerId + "binbashcrpktopiccreatebaseline"),
+	hashString(redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatebaseline"),
+	hashString(redpandaContainerId + "usrbinbashusrbinrpktopiccreatebaseline"),
+	hashString(redpandaContainerId + "optredpandalibexecrpktopiccreatebaseline"),
+	hashString(redpandaContainerId + "binbashcrpktopiccreatesignalminusbaseline"),
+	hashString(redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatesignalminusbaseline"),
+	hashString(redpandaContainerId + "usrbinbashusrbinrpktopiccreatesignalminusbaseline"),
+	hashString(redpandaContainerId + "optredpandalibexecrpktopiccreatesignalminusbaseline"),
+	hashString(redpandaContainerId + "binbashcrpktopiccreatetetragon"),
+	hashString(redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatetetragon"),
+	hashString(redpandaContainerId + "usrbinbashusrbinrpktopiccreatetetragon"),
+	hashString(redpandaContainerId + "optredpandalibexecrpktopiccreatetetragon"),
+	hashString(redpandaContainerId + "binbashcrpktopiccreatekind-smb"),
+	hashString(redpandaContainerId + "usrbinrpkbashusrbinrpktopiccreatekind-smb"),
+	hashString(redpandaContainerId + "usrbinbashusrbinrpktopiccreatekind-smb"),
+	hashString(redpandaContainerId + "optredpandalibexecrpktopiccreatekind-smb"),
+	hashString(redpandaContainerId + "binbashcmkdirptmpbaseline"),
+	hashString(redpandaContainerId + "usrbinmkdirptmpbaseline"),
+	hashString(redpandaContainerId + "usrbintestdtmpbaseline"),
+	hashString(redpandaContainerId + "usrbintarxmfCtmpbaseline"),
 }
