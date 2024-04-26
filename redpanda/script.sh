ac77a5764fcdae3822b0a3e33b0a581c573581ba
entlein <einentlein@gmail.com>
2024-04-26 16:03:51 +0200

    cleanup: finally a new Makefile for kind that automatically does the key magick, please test on non-MAC

--- redpanda/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-internal-rpk  topic consume keygen -f '%k\n' > /tmp/list
-sort -u /tmp/list  > /tmp/list.uniq
-#sed -i 's/.*/"&",/' list.uniq  ## LINUX
-sed -i '' 's/.*/"&",/' /tmp/list.uniq  ## MAC
-paste -s -d ' ' /tmp/list.uniq >> one_line_list.txt
-HASHLIST=$(cat one_line_list.txt )
-sed "s/\$HASHLIST/$HASHLIST/g" signal/keyless/transform.go > signal/transform.go
