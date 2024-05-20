e7f6e64f0723ffe6af0da7d4676f1818329f8f73
tobiasgrantner <tobias.grantner@gmail.com>
2024-05-20 20:55:14 +0200

    Transform unwrapping JSON string sent to signal topic

--- redpanda/signal/transform.go
+++ redpanda/signal/transform.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/redpanda-data/redpanda/src/transform-sdk/go/transform"
 )
 
@@ -8,13 +10,14 @@ func main() {
 	transform.OnRecordWritten(doTransform)
 }
 
-var value = []byte("null")
-
 func doTransform(e transform.WriteEvent, w transform.RecordWriter) error {
-	// Create a new record with null value
+	// Unescape value
+	value, _ := strconv.Unquote(string(e.Record().Value))
+
+	// Create a new record with unescaped value
 	record := &transform.Record{
 		Key:       e.Record().Key,
-		Value:     value,
+		Value:     []byte(value),
 		Offset:    e.Record().Offset,
 		Timestamp: e.Record().Timestamp,
 		Headers:   e.Record().Headers,
