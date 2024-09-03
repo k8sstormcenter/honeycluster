9d20c4e83ab982949b19ab38b057bf6c105838fc
tobiasgrantner <tobias.grantner@gmail.com>
2024-09-03 15:03:06 +0200

    Removed unused import

--- attacks/managedidentityazure/src/server.js
+++ attacks/managedidentityazure/src/server.js
@@ -4,7 +4,6 @@ import { SecretClient } from "@azure/keyvault-secrets";
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { execSync } from 'child_process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
