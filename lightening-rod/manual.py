a7fab1dd18a1a68c8efbc8ca89b1b008905dec09
entlein <einentlein@gmail.com>
2024-12-22 22:17:27 +0100

    adapt to fix: need new api for using different type of queue

--- lightening-rod/manual.py
+++ lightening-rod/manual.py
@@ -58,6 +58,13 @@ def convert_single_to_stix():
     stix = transform_single_tetragon_to_stix(tetragon_log)
     return stix, 200
 
+@app.route('/convert_list_to_stix', methods=['GET'])
+def convert_single_to_stix():
+    queue= request.args.get('queue')
+    tetragon_logs = client.lrange(queue, 0, -1)
+    transform_tetragon_to_stix(tetragon_logs)
+    return 200
+
 @app.route('/bundle_for_viz', methods=['GET'])
 def bundle_for_viz():
     individual_bundles = client.hgetall(REDIS_BUNDLEVISKEY)
