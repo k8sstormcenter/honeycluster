9b9bda575a2a974a413fa56e2b14e3eff943e732
entlein <einentlein@gmail.com>
2025-04-15 18:04:45 +0200

    feature: configuring kubescape for this demo

--- traces/kubescape-verify/attacks/webapp/setup.sh
+++ traces/kubescape-verify/attacks/webapp/setup.sh
@@ -7,7 +7,7 @@ killall kubectl 2>/dev/null
 
 # Apply the YAML file for the web app
 echo "[+] Applying YAML file for the web app"
-kubectl apply -f traces/kubescape-verify/attacks/webapp/webapp.yaml
+kubectl apply -f ~/honeycluster/traces/kubescape-verify/attacks/webapp/webapp.yaml
 
 # Wait for the web app to be ready
 echo "[+] Waiting for the web app to be ready"
@@ -16,10 +16,10 @@ kubectl wait --for=condition=ready pod -l app=ping-app
 # Port forward from port 80 to port localhost:58080
 echo "[+] Port forwarding from port 80 to localhost:58080"
 kubectl port-forward pod/ping-app 58080:80 2>&1 >/dev/null &
-sudo socat TCP-LISTEN:58080,bind=172.16.0.4,fork TCP:127.0.0.1:58080 &
+sudo socat TCP-LISTEN:58080,bind=172.16.0.2,reuseaddr,fork TCP:127.0.0.1:58080&
 
 
 # Wait for the port forward to be ready
 echo "[+] Waiting for the port forward to be ready"
 sleep 1
-echo "[+] The web app is ready"
+echo "[+] The web app is ready, you can curl it like so: curl -k 172.16.0.2:30156/ping.php?ip=172.16.0.2"
