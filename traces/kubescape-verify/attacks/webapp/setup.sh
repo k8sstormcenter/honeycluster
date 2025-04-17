586c05bcea19f61baabe87b7c24e6d3b671a50a0
entlein <einentlein@gmail.com>
2025-04-17 09:09:28 +0200

    fix: making the curl work in setup.sh out of the box

--- traces/kubescape-verify/attacks/webapp/setup.sh
+++ traces/kubescape-verify/attacks/webapp/setup.sh
@@ -16,10 +16,12 @@ kubectl wait --for=condition=ready pod -l app=webapp
 # Port forward from port 80 to port localhost:58080
 echo "[+] Port forwarding from port 80 to localhost:58080"
 kubectl port-forward pod/webapp 58080:80 2>&1 >/dev/null &
+export port=$(kubectl describe svc/webapp | grep NodePort | awk '{print $3}' | cut -d '/' -f1)
+echo "NodePort is: $port"
 #sudo socat TCP-LISTEN:58080,bind=172.16.0.2,reuseaddr,fork TCP:127.0.0.1:58080&
 
 
 # Wait for the port forward to be ready
 echo "[+] Waiting for the port forward to be ready"
 sleep 1
-echo "[+] The web app is ready, you can curl it like so: curl -k 172.16.0.2:30156/ping.php?ip=172.16.0.2"
+echo "[+] The web app is ready, you can curl it like so: curl -k 172.16.0.2:$port/ping.php?ip=172.16.0.2"
