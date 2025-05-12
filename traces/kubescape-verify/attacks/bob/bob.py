d9c0b61b07f3ab66773117cfb77bfcf4fac9d77a
entlein <einentlein@gmail.com>
2025-05-12 18:02:32 +0200

    cant see the difference
    Signed-off-by: entlein <einentlein@gmail.com>

--- traces/kubescape-verify/attacks/bob/bob.py
+++ traces/kubescape-verify/attacks/bob/bob.py
@@ -1,4 +1,5 @@
 import yaml
+import re
 import ipaddress
 
 def process_yaml(yaml_file, values):
@@ -45,3 +46,17 @@ def expand_cidr_in_execs(exec_blocks, cidr):
     exec_blocks.extend(new_exec_blocks)
 
 
+def read_values_from_file(values_file):
+    values = {}
+    with open(values_file, "r") as f:
+        for line in f:
+            # Use regex to handle potential spaces around the '='
+            match = re.match(r"([a-zA-Z0-9_-]+)\s*=\s*(.*)", line)
+            if match:
+                key, value = match.groups()
+                values[key] = value.strip()  # Remove leading/trailing whitespace
+    return values
+
+
+values = read_values_from_file("bob.values")
+process_yaml("bob.yaml", values)
