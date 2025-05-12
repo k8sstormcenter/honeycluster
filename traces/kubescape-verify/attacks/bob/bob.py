a997c4e76eaa03f7b4babd612c4e4a55b790e763
entlein <einentlein@gmail.com>
2025-05-12 20:49:59 +0200

    conversion in both directions
    Signed-off-by: entlein <einentlein@gmail.com>

--- traces/kubescape-verify/attacks/bob/bob.py
+++ traces/kubescape-verify/attacks/bob/bob.py
@@ -1,12 +1,50 @@
 import yaml
 import re
-import ipaddress
+
+
+def appProfile_to_Bob():
+    with open("bob_compare.yaml", "r") as f:
+        data = yaml.safe_load(f)
+
+    parameter_map = {
+        "namespace": data["metadata"]["namespace"],
+        "name": data["metadata"]["labels"]["kubescape.io/workload-name"],
+        "clustername": data["metadata"]["annotations"]["kubescape.io/wlid"].split("//")[1].split("/")[0].split("cluster-")[1], # Extract clustername from wlid
+        "templatehash": data["metadata"]["labels"]["kubescape.io/instance-template-hash"],
+        "workloadkind": data["metadata"]["labels"]["kubescape.io/workload-kind"].lower(),
+        "camelworkloadkind": data["metadata"]["labels"]["kubescape.io/workload-kind"],
+        "instancekind": data["metadata"]["name"].split("-")[0],
+        "camelinstancekind": data["kind"],
+    }
+
+    output_file = "bob_generated.values"
+    with open(output_file, "w") as f:
+        for key, value in parameter_map.items():
+            f.write(f"{key}={value}\n")
+    print(f"Processed BoB Values written to {output_file}")
+
+
+    data["metadata"]["annotations"]["kubescape.io/instance-id"] = "apiVersion-apps/v1/namespace-$values.namespace/kind-$values.camelinstancekind/name-$values.name-$values.templatehash"
+    data["metadata"]["annotations"]["kubescape.io/wlid"] = "wlid://cluster-$values.clustername/namespace-$values.namespace/$values.workloadkind-$values.name"
+    data["metadata"]["labels"]["kubescape.io/workload-kind"] = "$values.camelworkloadkind"
+    data["metadata"]["labels"]["kubescape.io/workload-name"] = "$values.name"
+    data["metadata"]["labels"]["kubescape.io/workload-namespace"] = "$values.namespace"
+    data["metadata"]["name"] = "$values.instancekind-$values.name-$values.templatehash"
+    data["metadata"]["namespace"] = "$values.namespace"
+    data["resourceVersion"] = '"1"' 
+
+
+    output_file = "bob_generated.yaml"
+    with open(output_file, "w") as f:
+        yaml.dump(data, f, indent=2)
+    print(f"Processed BoB YAML written to {output_file}")
+
 
 def process_yaml(yaml_file, values):
     with open(yaml_file, "r") as f:
         yaml_content = f.read()
 
-    # Substitute values in the entire YAML content
+
     for key, value in values.items():
         print(key, value)
         if key.islower(): 
@@ -34,5 +72,5 @@ def read_values_from_file(values_file):
     return values
 
 
-values = read_values_from_file("bob.values")
-process_yaml("bob.yaml", values)
+values = read_values_from_file("bob_generated.values")
+process_yaml("bob_generated.yaml", values)
