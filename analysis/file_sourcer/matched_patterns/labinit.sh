512cce63103a426a571f24b3ac52e604a4fa2940
Mehmet Berk Gürçay <mehmet.gurcay@danfoss.com>
2025-07-16 08:24:18 +0200

    fix typo in script

--- analysis/file_sourcer/matched_patterns/labinit.sh
+++ analysis/file_sourcer/matched_patterns/labinit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-input_file="/honeycluster/analysis/file_sourcer/tetragon/stix_bundle_schema_init.json"
+input_file="/honeycluster/analysis/file_sourcer/matched_patterns/matched_patterns_schema_init.json"
 output_file="/tmp/matched_patterns.json"
 
 mv $input_file $output_file
