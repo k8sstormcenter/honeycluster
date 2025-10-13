cd49a3c99163dc25f898134c93ba9d738517bf87
entlein <einentlein@gmail.com>
2025-10-13 18:21:24 +0200

    new bob for pixie
    Signed-off-by: entlein <einentlein@gmail.com>

--- honeystack/pixie/pixie_vizier/sbob/getprofiles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-namespace="pl"
-list=$(kubectl get applicationprofiles.spdx.softwarecomposition.kubescape.io -n $namespace| grep -v NAME| cut -d ' ' -f1)
-for file in $list; do
-    kubectl get -n $namespace applicationprofiles  $file -o yaml >~/bob$file.yaml ;
-done
