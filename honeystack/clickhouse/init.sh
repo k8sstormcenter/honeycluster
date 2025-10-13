954204f7d3cfd8d87f5f0a733bba5d934dad1f83
entlein <einentlein@gmail.com>
2025-10-13 14:28:59 +0200

    prepare kcna -> namespace exclusions for lab
    Signed-off-by: entlein <einentlein@gmail.com>

--- honeystack/clickhouse/init.sh
+++ honeystack/clickhouse/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-podname=$(kubectl get pods -n clickhouse -l app=clickhouse -o jsonpath='{.items[0].metadata.name}')
-kubectl exec -n clickhouse $podname -- clickhouse-client --multiquery --database=default <<'EOF'
+podname=$(kubectl get pods -n honey -l app=clickhouse -o jsonpath='{.items[0].metadata.name}')
+kubectl exec -n honey $podname -- clickhouse-client --multiquery --database=default <<'EOF'
 CREATE TABLE IF NOT EXISTS default.kubescape_logs (
     BaseRuntimeMetadata String,
     CloudMetadata String,
