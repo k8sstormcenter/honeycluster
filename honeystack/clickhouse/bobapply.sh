232163acb5f8d6760b7bd4bd6bd0e5d9ec1ac83d
entlein <einentlein@gmail.com>
2025-10-22 14:57:54 +0200

    change in strategy the labels need to be attached to the original manifest, else this is just a mess
    Signed-off-by: entlein <einentlein@gmail.com>

--- honeystack/clickhouse/bobapply.sh
+++ honeystack/clickhouse/bobapply.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ns=click
 kubectl create ns $ns
-for file in click*.yaml; do
+for file in honeystack/clickhouse/click*.yaml; do
     kubectl apply -f $file;
 done
 
-kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-app" kubescape.io/user-defined-profile=app
-kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-clickhouse " kubescape.io/user-defined-profile="clickhouse"
-kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-mongodb" kubescape.io/user-defined-profile=mongo
-kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-otel-collector" kubescape.io/user-defined-profile=otel
+#kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-app" kubescape.io/user-defined-profile=app
+#kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-clickhouse " kubescape.io/user-defined-profile="clickhouse"
+#kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-mongodb" kubescape.io/user-defined-profile=mongo
+#kubectl label --overwrite -n $ns "deployment/hyperdx-hdx-oss-v2-otel-collector" kubescape.io/user-defined-profile=otel
